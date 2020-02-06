cab03471205af1fb40e824c406d99466816c6503
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2020-02-06 13:15:33 -0500

    Install standardJS and fix all linting errors

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Nainativu CDS`,
-    description: `Nainativu Canadian Development Society`,
-    author: `Jana Rajakumar`,
-    siteUrl: `https://nainativucds.org`
+    title: 'Nainativu CDS',
+    description: 'Nainativu Canadian Development Society',
+    author: 'Jana Rajakumar',
+    siteUrl: 'https://nainativucds.org'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `ncds`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
+        name: 'ncds',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
